aa658871015f48ef66a4b79b1bd161061039aff7
Mahmood Garibov <mahmood.garibov@gmail.com>
2022-02-14 23:34:29 +0400

    Register view added

--- backend/src/Kanban.Presentation/Controllers/AccountController.cs
+++ backend/src/Kanban.Presentation/Controllers/AccountController.cs
@@ -8,5 +8,10 @@ namespace Kanban.Presentation.Controllers
         {
             return View();
         }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
     }
 }
---
+++ backend/src/Kanban.Presentation/ViewModels/Account/RegisterViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kanban.Presentation.ViewModels.Account
+{
+    public class RegisterViewModel
+    {
+        [Display(Name = "Full name")]
+        public string FullName { get; set; }
+
+        [Display(Name = "Organisation")]
+        public string Organisation { get; set; }
+
+        [Display(Name = "Phone number")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        public string ConfirmPassword { get; set; }
+
+        
+    }
+}
