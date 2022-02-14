90f8666945196af198a4141983a7298bc069b4f8
Mahmood Garibov <mahmood.garibov@gmail.com>
2022-02-14 19:18:09 +0400

    User repository completed

---
+++ backend/src/Kanban.DataAccess/Repositories/Abstracts/IUserRepository.cs
@@ -0,0 +1,37 @@
+﻿using Kanban.Core.Entities;
+using Kanban.DataAccess.Abstracts.Base;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kanban.DataAccess.Repositories.Abstracts
+{
+    public interface IUserRepository
+    {
+        Task<List<User>> GetAllAsync();
+        Task<User> GetUserAsync(ClaimsPrincipal user);
+        Task<IdentityResult> UpdateAsync(User user);
+        Task<IdentityResult> CreateAsync(User user, string passsword);
+        Task<IdentityResult> DeleteAsync(User user);
+        Task<User> FindByUserNameAsync(string username);
+        Task<User> FindByIdAsync(string username);
+        Task<User> FindByEmailAsync(string email);
+        Task<bool> IsInRoleAsync(User user, string role);
+        Task<IdentityResult> AddToRoleAsync(User user, string role);
+        Task<IdentityResult> AddToRolesAsync(User user, IEnumerable<string> roles);
+        Task<string[]> GetUserRolesId(User user);
+        Task<IList<string>> GetRolesAsync(User user);
+        Task<IdentityResult> RemoveFromRolesAsync(User user, IEnumerable<string> roles);
+        Task<string> GenerateEmailConfirmationTokenAsync(User user);
+        Task<IdentityResult> ConfirmEmailAsync(User user, string token);
+        Task<bool> CheckPasswordAsync(User user, string token);
+        Task<bool> IsEmailConfirmedAsync(User user);
+        Task<string> GeneratePasswordResetTokenAsync(User user);
+        Task<IdentityResult> ResetPasswordAsync(User user, string token, string password);
+        Task<IdentityResult> ChangePasswordAsync(User user, string currentPassword, string newPassword);
+    }
+}
---
+++ backend/src/Kanban.DataAccess/Repositories/Implementations/UserRepository.cs
@@ -0,0 +1,123 @@
+﻿using Kanban.Core.Entities;
+using Kanban.DataAccess.Persistance.Contexts;
+using Kanban.DataAccess.Repositories.Abstracts;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kanban.DataAccess.Repositories.Implementations
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly KanbanContext _context;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UserRepository(
+            KanbanContext context,
+            UserManager<User> userManager,
+            SignInManager<User> signInManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<List<User>> GetAllAsync()
+        {
+            return await _userManager.Users.ToListAsync();
+        }
+        public async Task<User> GetUserAsync(ClaimsPrincipal User)
+        {
+            return await _userManager.GetUserAsync(User);
+        }
+        public async Task<IdentityResult> UpdateAsync(User user)
+        {
+            return await _userManager.UpdateAsync(user);
+        }
+        public async Task<IdentityResult> CreateAsync(User user, string password)
+        {
+            return await _userManager.CreateAsync(user, password);
+        }
+        public async Task<IdentityResult> DeleteAsync(User user)
+        {
+            return await _userManager.DeleteAsync(user);
+        }
+        public async Task<User> FindByUserNameAsync(string username)
+        {
+            return await _userManager.FindByNameAsync(username);
+
+        }
+        public async Task<User> FindByIdAsync(string userId)
+        {
+            return await _userManager.FindByIdAsync(userId);
+        }
+        public async Task<User> FindByEmailAsync(string email)
+        {
+            return await _userManager.FindByEmailAsync(email);
+        }
+        public async Task<bool> IsInRoleAsync(User user, string role)
+        {
+            return await _userManager.IsInRoleAsync(user, role);
+        }
+        public async Task<IdentityResult> AddToRoleAsync(User user, string role)
+        {
+            return await _userManager.AddToRoleAsync(user, role);
+        }
+        public async Task<IdentityResult> AddToRolesAsync(User user, IEnumerable<string> roles)
+        {
+            return await _userManager.AddToRolesAsync(user, roles);
+        }
+        public async Task<string[]> GetUserRolesId(User user)
+        {
+            var userRoles = await _userManager.GetRolesAsync(user);
+            string[] roleIDs = userRoles.Select(r => _roleManager.FindByNameAsync(r).Result.Id).ToArray();
+
+            return roleIDs;
+        }
+        public async Task<IList<string>> GetRolesAsync(User user)
+        {
+            return await _userManager.GetRolesAsync(user);
+        }
+        public async Task<IdentityResult> RemoveFromRolesAsync(User user, IEnumerable<string> roles)
+        {
+            return await _userManager.RemoveFromRolesAsync(user, roles);
+        }
+        public async Task<string> GenerateEmailConfirmationTokenAsync(User user)
+        {
+            return await _userManager.GenerateEmailConfirmationTokenAsync(user);
+        }
+        public async Task<IdentityResult> ConfirmEmailAsync(User user, string token)
+        {
+            return await _userManager.ConfirmEmailAsync(user, token);
+        }
+        public async Task<bool> CheckPasswordAsync(User user, string token)
+        {
+            return await _userManager.CheckPasswordAsync(user, token);
+        }
+        public async Task<bool> IsEmailConfirmedAsync(User user)
+        {
+            return await _userManager.IsEmailConfirmedAsync(user);
+        }
+        public async Task<string> GeneratePasswordResetTokenAsync(User user)
+        {
+            return await _userManager.GeneratePasswordResetTokenAsync(user);
+        }
+        public async Task<IdentityResult> ResetPasswordAsync(User user, string token, string password)
+        {
+            return await _userManager.ResetPasswordAsync(user, token, password);
+        }
+        public async Task<IdentityResult> ChangePasswordAsync(User user, string currentPassword, string newPassword)
+        {
+            return await _userManager.ChangePasswordAsync(user, currentPassword, newPassword);
+        }
+    }
+}
