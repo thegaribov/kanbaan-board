2989d9a57fd98b87ff83fea963247848682bde7b
Mahmood Garibov <mahmood.garibov@gmail.com>
2022-02-14 23:21:35 +0400

    Login view added

--- backend/src/Kanban.Presentation/Startup.cs
+++ backend/src/Kanban.Presentation/Startup.cs
@@ -15,12 +15,26 @@ namespace Kanban.Presentation
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public bool IsProduction { get; set; }
+        public bool IsDevelopment { get; set; }
+        public bool IsStaging { get; set; }
+        public bool IsDevelopmentWithDocker { get; set; }
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration) :this()
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public Startup()
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            IsProduction = Environments.Production == environment;
+            IsDevelopment = Environments.Development == environment;
+            IsStaging = Environments.Staging == environment;
+            IsDevelopmentWithDocker = "DevelopmentWithDocker" == environment;
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -31,14 +45,27 @@ namespace Kanban.Presentation
 
             services.AddDbContext<KanbanContext>(opt =>
             {
-                string serverName = Environment.GetEnvironmentVariable("POSTGRE_SERVER_NAME");
-                string serverPort = Environment.GetEnvironmentVariable("POSTGRE_SERVER_PORT");
-                string databaseName = Environment.GetEnvironmentVariable("POSTGRE_DB_NAME");
-                string userId = Environment.GetEnvironmentVariable("POSTGRE_USER_ID");
-                string userPassword = Environment.GetEnvironmentVariable("POSTGRE_USER_PASSWORD");
-                string maxOurPrepare = Environment.GetEnvironmentVariable("POSTGRE_MAX_AUTO_PREPARE");
+                var dbConnectionString = string.Empty;
 
-                var dbConnectionString = @$"Server={serverName};Port={serverPort};Database={databaseName};User Id={userId};Password={userPassword};MaxAutoPrepare={maxOurPrepare}";
+                if (IsDevelopment)
+                {
+                    //Environment.GetEnvironmentVariable("CONNECTION_STRING_NAME")
+                    //Production
+
+                    dbConnectionString = Configuration.GetConnectionString("Development");
+                }
+                else
+                {
+                    string serverName = Environment.GetEnvironmentVariable("POSTGRE_SERVER_NAME");
+                    string serverPort = Environment.GetEnvironmentVariable("POSTGRE_SERVER_PORT");
+                    string databaseName = Environment.GetEnvironmentVariable("POSTGRE_DB_NAME");
+                    string userId = Environment.GetEnvironmentVariable("POSTGRE_USER_ID");
+                    string userPassword = Environment.GetEnvironmentVariable("POSTGRE_USER_PASSWORD");
+                    string maxOurPrepare = Environment.GetEnvironmentVariable("POSTGRE_MAX_AUTO_PREPARE");
+
+                    dbConnectionString = @$"Server={serverName};Port={serverPort};Database={databaseName};User Id={userId};Password={userPassword};MaxAutoPrepare={maxOurPrepare}";
+                }
+
 
                 opt.UseNpgsql(dbConnectionString, options => {
                     options.UseNetTopologySuite();
---
+++ backend/src/Kanban.Presentation/ViewModels/Account/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kanban.Presentation.ViewModels.Account
+{
+    public class LoginViewModel
+    {
+        [Display(Name ="Email")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
