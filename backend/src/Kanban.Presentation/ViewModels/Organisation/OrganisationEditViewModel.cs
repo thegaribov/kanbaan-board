53f866e1652c3263a7f9c1760f7279c632cd4cc0
Mahmood Garibov <mahmood.garibov@gmail.com>
2022-02-15 18:39:36 +0400

    Organisation edit viewmodel's fluent validation added

--- backend/src/Kanban.Presentation/ViewModels/Organisation/OrganisationEditViewModel.cs
+++ backend/src/Kanban.Presentation/ViewModels/Organisation/OrganisationEditViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace Kanban.Presentation.ViewModels.Organisation
+﻿using FluentValidation;
+
+namespace Kanban.Presentation.ViewModels.Organisation
 {
     public class OrganisationEditViewModel
     {
@@ -6,5 +8,69 @@
         public string Name { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
+    }
+
+
+    public class OrganisationEditViewModelValidator : AbstractValidator<OrganisationEditViewModel>
+    {
+        public OrganisationEditViewModelValidator()
+        {
+            IntegrateRules();
+        }
+
+        private void IntegrateRules()
+        {
+
+            #region Organisation
+
+            RuleFor(model => model.Name)
+                .Cascade(CascadeMode.Stop)
+
+                .NotEmpty()
+                .WithMessage("Name can't be empty")
+
+                .NotNull()
+                .WithMessage("Name can't be empty")
+
+                .MinimumLength(3)
+                .WithMessage("Name min length can 3")
+
+                .MaximumLength(35)
+                .WithMessage("Name max length can 35");
+
+            #endregion
+
+            #region PhoneNumber
+
+            RuleFor(model => model.PhoneNumber)
+                .Cascade(CascadeMode.Stop)
+
+                .NotEmpty()
+                .WithMessage("Phone number can't be empty")
+
+                .NotNull()
+                .WithMessage("Phone number can't be empty");
+
+            #endregion
+
+            #region Address
+
+            RuleFor(model => model.Address)
+                .Cascade(CascadeMode.Stop)
+
+                .NotEmpty()
+                .WithMessage("Address can't be empty")
+
+                .NotNull()
+                .WithMessage("Address can't be empty")
+
+                .MinimumLength(3)
+                .WithMessage("Address min length can 3")
+
+                .MaximumLength(40)
+                .WithMessage("Address max length can 40");
+
+            #endregion
+        }
     }
 }
