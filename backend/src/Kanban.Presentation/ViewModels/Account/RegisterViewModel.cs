3c52f85e17c236e9b10e11d458fb7bedd045e3e8
Mahmood Garibov <mahmood.garibov@gmail.com>
2022-02-15 13:00:02 +0400

    FluentValidation for register viewmodel completed

--- backend/src/Kanban.Presentation/ViewModels/Account/RegisterViewModel.cs
+++ backend/src/Kanban.Presentation/ViewModels/Account/RegisterViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using FluentValidation;
+using System.ComponentModel.DataAnnotations;
 
 namespace Kanban.Presentation.ViewModels.Account
 {
@@ -28,5 +29,132 @@ namespace Kanban.Presentation.ViewModels.Account
         public string ConfirmPassword { get; set; }
 
         
+    }
+
+    public class RegisterViewModelValidator : AbstractValidator<RegisterViewModel>
+    {
+        public RegisterViewModelValidator()
+        {
+            IntegrateRules();
+        }
+
+        private void IntegrateRules()
+        {
+            #region Fullname
+
+            RuleFor(model => model.FullName)
+                .Cascade(CascadeMode.Stop)
+
+                .NotEmpty()
+                .WithMessage("Fullname can't be empty")
+
+                .NotNull()
+                .WithMessage("Fullname can't be empty")
+
+                .MinimumLength(3)
+                .WithMessage("Fullname min length can 3")
+
+                .MaximumLength(25)
+                .WithMessage("Fullname max length can 25");
+
+            #endregion
+
+            #region Organisation
+
+            RuleFor(model => model.Organisation)
+                .Cascade(CascadeMode.Stop)
+
+                .NotEmpty()
+                .WithMessage("Organisation can't be empty")
+
+                .NotNull()
+                .WithMessage("Organisation can't be empty")
+
+                .MinimumLength(3)
+                .WithMessage("Organisation min length can 3")
+
+                .MaximumLength(35)
+                .WithMessage("Organisation max length can 35");
+
+            #endregion
+
+            #region PhoneNumber
+
+            RuleFor(model => model.PhoneNumber)
+                .Cascade(CascadeMode.Stop)
+
+                .NotEmpty()
+                .WithMessage("Phone number can't be empty")
+
+                .NotNull()
+                .WithMessage("Phone number can't be empty");
+
+            #endregion
+
+            #region Address
+
+            RuleFor(model => model.Address)
+                .Cascade(CascadeMode.Stop)
+
+                .NotEmpty()
+                .WithMessage("Address can't be empty")
+
+                .NotNull()
+                .WithMessage("Address can't be empty")
+
+                .MinimumLength(3)
+                .WithMessage("Address min length can 3")
+
+                .MaximumLength(40)
+                .WithMessage("Address max length can 40");
+
+            #endregion
+
+            #region Email
+
+            RuleFor(model => model.Email)
+                .Cascade(CascadeMode.Stop)
+
+                .NotEmpty()
+                .WithMessage("Email can't be empty")
+
+                .NotNull()
+                .WithMessage("Email can't be empty")
+
+                .EmailAddress()
+                .WithMessage("Email is not in correct format");
+
+
+            #endregion
+
+            #region Password
+
+            RuleFor(model => model.Password)
+                .Cascade(CascadeMode.Stop)
+
+                .NotEmpty()
+                .WithMessage("Password can't be empty")
+
+                .NotNull()
+                .WithMessage("Password can't be empty");
+
+            #endregion
+
+            #region ConfirmPassword
+
+            RuleFor(model => model.ConfirmPassword)
+                .Cascade(CascadeMode.Stop)
+
+                .NotEmpty()
+                .WithMessage("Confirm password can't be empty")
+
+                .NotNull()
+                .WithMessage("Confirm password can't be empty")
+
+                .Equal(model => model.Password)
+                .WithMessage("Passwords don't match");
+
+            #endregion
+        }
     }
 }
