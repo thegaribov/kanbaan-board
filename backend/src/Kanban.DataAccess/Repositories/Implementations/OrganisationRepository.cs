41868bf9ba5e246326abca77e10e2f9f982406d0
Mahmood Garibov <mahmood.garibov@gmail.com>
2022-02-15 13:45:38 +0400

    Organisation repository completed

---
+++ backend/src/Kanban.DataAccess/Repositories/Implementations/OrganisationRepository.cs
@@ -0,0 +1,26 @@
+﻿using Kanban.Core.Entities;
+using Kanban.DataAccess.Persistance.Contexts;
+using Kanban.DataAccess.Repositories.Abstracts;
+using Kanban.DataAccess.Repositories.Implementations.Base;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kanban.DataAccess.Repositories.Implementations
+{
+    public class OrganisationRepository : EFBaseRepository<Organisation>, IOrganisationRepository
+    {
+        private readonly KanbanContext _context;
+
+        public OrganisationRepository(KanbanContext context)
+            :base(context)
+        {
+            _context = context;
+        }
+    }
+}
