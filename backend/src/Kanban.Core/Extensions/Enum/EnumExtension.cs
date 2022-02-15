f772754b0c5d52022153b445a9b918a81d07db30
Mahmood Garibov <mahmood.garibov@gmail.com>
2022-02-15 20:19:53 +0400

    Enum extension added

---
+++ backend/src/Kanban.Core/Extensions/Enum/EnumExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kanban.Core.Extensions.Enum
+{
+    public static class EnumExtension
+    {
+        public static string GetDisplayName(this System.Enum enumType)
+        {
+            return enumType
+                        .GetType()
+                        .GetMember(enumType.ToString())
+                        .First()
+                        .GetCustomAttribute<DisplayAttribute>()
+                        .Name;
+        }
+    }
+}
