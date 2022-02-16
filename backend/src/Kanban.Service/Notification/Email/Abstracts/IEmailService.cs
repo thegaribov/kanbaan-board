d5628708d3e18b5b2cff60e0b2191d4f97a26312
Mahmood Garibov <mahmood.garibov@gmail.com>
2022-02-16 14:22:56 +0400

    Email service added

---
+++ backend/src/Kanban.Service/Notification/Email/Abstracts/IEmailService.cs
@@ -0,0 +1,42 @@
+﻿using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kanban.Service.Notification.Email.Abstracts
+{
+    public interface IEmailService
+    {
+        public class Message
+        {
+            public List<MailboxAddress> Receivers { get; set; } = new List<MailboxAddress>();
+            public string Subject { get; set; }
+            public string Body { get; set; }
+
+            public Message(string subject, string body, List<string> receivers)
+            {
+                Subject = subject;
+                Body = body;
+                Receivers.AddRange(receivers?.Select(recipent => new MailboxAddress(string.Empty, recipent)));
+            }
+        }
+
+        Task<bool> SendAsync(Message message);
+        Task<bool> SendAsync(string subject, string body, params string[] receivers)
+        {
+            var message = new Message(subject, body, receivers.ToList());
+
+            return SendAsync(message);
+        }
+
+        void SendInBackground(Message message);
+        void SendInBackground(string subject, string body, params string[] receivers)
+        {
+            var message = new Message(subject, body, receivers.ToList());
+
+            SendInBackground(message);
+        }
+    }
+}
---
+++ backend/src/Kanban.Service/Notification/Email/Implementations/SMTP/SMTP.cs
@@ -0,0 +1,110 @@
+﻿using Kanban.Service.Notification.Email.Abstracts;
+using Microsoft.Extensions.Logging;
+using MimeKit;
+using MailKit.Net.Smtp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kanban.Service.Infrastructure.BackgroundTask.BackgroundTaskQueue.Abstracts;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kanban.Service.Notification.Email.Implementations.SMTP
+{
+    public class Smtp : IEmailService
+    {
+        ///If your SMTP host is gmail then don't forget to
+        ///enable "Less secure app access" from settings
+        private readonly string _senderEmail;
+        private readonly string _senderEmailPassword;
+        private readonly string _senderHost;
+        private readonly int _port;
+
+        private readonly ILogger<Smtp> _logger;
+        private readonly IBackgroundTaskQueue _backgroundTaskQueue;
+
+        public Smtp(
+            ILogger<Smtp> logger,
+            IBackgroundTaskQueue backgroundTaskQueue)
+
+        {
+            _senderEmail = Environment.GetEnvironmentVariable("SMTP_SENDER_EMAIL");
+            _senderEmailPassword = Environment.GetEnvironmentVariable("SMTP_SENDER_EMAIL_PASSWORD");
+            _senderHost = Environment.GetEnvironmentVariable("SMTP_SENDER_HOST");
+            _port = int.Parse(Environment.GetEnvironmentVariable("SMTP_PORT"));
+
+            _logger = logger;
+            _backgroundTaskQueue = backgroundTaskQueue;
+        }
+
+        public async Task<bool> SendAsync(IEmailService.Message message)
+        {
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress(string.Empty, _senderEmail));
+            emailMessage.To.AddRange(message.Receivers);
+            emailMessage.Subject = message.Subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = message.Body };
+
+            return await SendEmailAsync(emailMessage);
+        }
+        private async Task<bool> SendEmailAsync(MimeMessage mailMessage)
+        {
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    await client.ConnectAsync(_senderHost, _port, true);
+                    client.AuthenticationMechanisms.Remove("XOAUTH2");
+                    await client.AuthenticateAsync(_senderEmail, _senderEmailPassword);
+
+                    var result = await client.SendAsync(mailMessage);
+
+                    return true;
+                }
+                catch (Exception smtpException)
+                {
+                    _logger.LogError(smtpException, $"[{nameof(Smtp)}] [UTC] [{DateTime.UtcNow.ToString("dd/MM/yyy HH:mm:ss")}] Send email unsuccessfully completed, exception occurred.");
+
+                    return false;
+                }
+                finally
+                {
+                    await client.DisconnectAsync(true);
+                    client.Dispose();
+                }
+            }
+        }
+
+        public void SendInBackground(IEmailService.Message message)
+        {
+            _backgroundTaskQueue.EnqueueTask(async (serviceScopeFactory, cancellationToken) =>
+            {
+                // Get services
+                using var scope = serviceScopeFactory.CreateScope();
+                var smtpEmailService = scope.ServiceProvider.GetRequiredService<Smtp>();
+                var logger = scope.ServiceProvider.GetRequiredService<ILogger<Smtp>>();
+
+                try
+                {
+                    var isEmailSendSuccess = await smtpEmailService.SendAsync(message);
+
+                    if (isEmailSendSuccess)
+                    {
+                        logger.LogInformation($"[BT] [UTC] [{DateTime.UtcNow.ToString("dd/MM/yyy HH:mm:ss")}] Send email completed successfully.");
+                    }
+                    else
+                    {
+                        logger.LogWarning($"[BT] [UTC] [{DateTime.UtcNow.ToString("dd/MM/yyy HH:mm:ss")}] Send email completed unsuccessfully, there are some problem, please check it");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, $"[BT] [UTC] [{DateTime.UtcNow.ToString("dd/MM/yyy HH:mm:ss")}] Send email completed unsuccessfully, exception occurred.");
+                }
+            });
+        }
+
+    }
+}
