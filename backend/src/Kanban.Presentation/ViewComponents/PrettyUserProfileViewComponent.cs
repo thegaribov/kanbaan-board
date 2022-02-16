14b66152e87cad5a25ab8a325e04ca17f044fcfc
Mahmood Garibov <mahmood.garibov@gmail.com>
2022-02-16 22:48:07 +0400

    Pretty profile image completed

---
+++ backend/src/Kanban.Presentation/ViewComponents/PrettyUserProfileViewComponent.cs
@@ -0,0 +1,41 @@
+﻿using Kanban.Core.Helpers.ActionResultMessage;
+using Kanban.Presentation.ViewModels.ViewComponent;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kanban.Presentation.ViewComponents
+{
+    public class PrettyUserProfileViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(string fullName, string size = "sm")
+        {
+            var model = new PrettyProfileViewModel
+            {
+                Size = size
+            };
+
+            try
+            {
+                var names = fullName.Split(" ");
+                StringBuilder resultName = new StringBuilder();
+
+                foreach (var name in names)
+                {
+                    resultName.Append(name.ToUpperInvariant()[0]);
+                }
+
+                model.FullNameFirstLetter = resultName.ToString();
+
+                return View("PrettyUserProfile", model);
+            }
+            catch
+            {
+                model.FullNameFirstLetter = string.Empty;
+
+                return View("PrettyUserProfile", model);
+            }
+        }
+    }
+}
---
+++ backend/src/Kanban.Presentation/ViewModels/ViewComponent/PrettyProfileViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Kanban.Presentation.ViewModels.ViewComponent
+{
+    public class PrettyProfileViewModel
+    {
+        public string FullNameFirstLetter { get; set; }
+        public string Size { get; set; }
+    }
+}
