329795521f6003d110d7feb554981e5406c2c26f
Mahmood Garibov <mahmood.garibov@gmail.com>
2022-02-17 14:02:36 +0400

    https redirection removed

--- backend/src/Kanban.Presentation/Startup.cs
+++ backend/src/Kanban.Presentation/Startup.cs
@@ -96,9 +96,9 @@ namespace Kanban.Presentation
             {
                 options.Password.RequireLowercase = false;
                 options.Password.RequireUppercase = false;
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireDigit = false;
-                options.Password.RequiredLength = 0;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 6;
                 options.User.RequireUniqueEmail = true;
                 //options.SignIn.RequireConfirmedEmail = true;
             })
@@ -166,7 +166,7 @@ namespace Kanban.Presentation
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+
             app.UseStaticFiles();
 
             app.UseRouting();
